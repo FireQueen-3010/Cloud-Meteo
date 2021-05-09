62a68570f5d3b57346f8cd6807647b31f73a32d8
FireQueen-3010 <dikshapatro807@gmail.com>
2021-05-09 17:17:40 +0530

    Made few changes

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const weatherRouter = require("./routers/weather.route")
 const app = express();
-
+const port = process.env.PORT || 8000
 app.use(weatherRouter);
 
+app.get("",(req,res)=>{
+  res.send("Proceed to /weather to find your weather")
+})
 
-app.listen(8000, () => {
-  console.log("Server is up and running on port 8000");
+app.listen(port, () => {
+  console.log("Server is up and running on port "+port);
 });
--- backend/routers/weather.route.js
+++ backend/routers/weather.route.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mainforecast = require("../utils/mainforecast");
 
-
-router.get("", (req, res) => {
+router.get("/weather", (req, res) => {
   if (!req.query.address)
     return res.send({
       error: "Please provide an address!",
