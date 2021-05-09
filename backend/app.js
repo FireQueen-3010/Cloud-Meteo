4696c0e49961631a1d758c18edce65212867a954
FireQueen-3010 <dikshapatro807@gmail.com>
2021-05-09 17:37:12 +0530

    Made few changes

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const weatherRouter = require("./routers/weather.route")
 const app = express();
 const port = process.env.PORT || 8000
-app.use(weatherRouter);
 
-app.get("",(req,res)=>{
+
+app.get("/",(req,res)=>{
   res.send("Proceed to /weather to find your weather")
 })
+
+app.use(weatherRouter);
 
 app.listen(port, () => {
   console.log("Server is up and running on port "+port);
