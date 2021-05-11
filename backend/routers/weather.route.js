a7370ab0cfcab892134e7d7f683b7b12067ff337
FireQueen-3010 <dikshapatro807@gmail.com>
2021-05-11 13:09:29 +0530

    Changes

--- backend/routers/weather.route.js
+++ backend/routers/weather.route.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 const mainforecast = require("../utils/mainforecast");
 
-router.get("", (req, res) => {
+router.get("/", (req, res) => {
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
   if (!req.query.address)
     return res.send({
       error: "Please provide an address!",
