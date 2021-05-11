99446c48df14a54a895b0993c0d6f0345e539c64
FireQueen-3010 <dikshapatro807@gmail.com>
2021-05-11 11:21:19 +0530

    Made few changes

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const weatherRouter = require("./routers/weather.route")
 const app = express();
 const port = process.env.PORT || 8000
 
+app.use(cors({
+  origin: "http://localhost:3000/"
+  }));
+  
 app.get("/",(req,res)=>{
   res.send("Proceed to /weather to find your weather")
 })
