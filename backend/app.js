5931dd73f0defa48130a8563bdec01ae592acc92
FireQueen-3010 <dikshapatro807@gmail.com>
2021-05-11 20:28:03 +0530

    Changes

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,12 @@ const cors = require("cors");
 const weatherRouter = require("./routers/weather.route")
 const app = express();
 const port = process.env.PORT || 8000
-require('dotenv').config();
 
-app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:3000/"
+  }));
   
-// app.get("/",(req,res)=>{
-//   res.send("Proceed to /weather to find your weather")
-// })
 
 app.use(weatherRouter);
 
