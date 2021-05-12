f0d57e61b6e6c2bcf0ff428e83598f3280f1a3f3
FireQueen-3010 <dikshapatro807@gmail.com>
2021-05-12 17:47:22 +0530

    Optimised

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -1,25 +1,37 @@
 import {useState} from 'react';
 const axios = require('axios');
-
 const Search = ({text, btnText, placeholder, href}) =>{
     const [query,setQuery]=useState("");
-
+    const [ret, setRet]=useState("");
+    const [data, setData]=useState("");
+    var Data="";
    const handleSubmit = (e) =>{
     e.preventDefault();
       axios.get(`https://cloud-meteo.herokuapp.com/?address=${query}`).then((response) => {
-         console.log(response.data);
-     })
+         Data=response.data;  
+         setRet("Found")
+         setData(Data);
+     }).catch((err)=>{
+         setRet("Error")
+        })
+     
     }
-
 return(
     <div className="search">
      <h1>{text}</h1>
      <div className="search--div">
      <form onSubmit={handleSubmit} className="search--div-form">
      <input onChange={(e) => setQuery(e.target.value)} className="search--div-field" type="search" placeholder={placeholder} value={query}/>
-     <button className="search--div-btn">{btnText}</button>
+     <button onClick={(e)=> setRet("Sent")} className="search--div-btn">{btnText}</button>
      </form>
      </div>
+     {ret==="Sent"&&(<div className="result--div"><h2 className="result--div-head">Loading your weather...</h2></div>)}
+     {ret==="Error"&&(<div className="result--div"><h2 className="result--div-head">Something went wrong...is it the location or your internet?</h2></div>)}
+     {ret==="Found"&&(<div className="result--div">
+     <h2 className="result--div-head">Location: {data.address}</h2>
+     <h2 className="result--div-head2">{data.forecast}</h2>
+     </div>)}
+    
     </div>
 )
 }
